fc9c254c66d4e9b74c6c176debf0b3d9acae4a5d
Yawning Angel <yawning@schwanenlied.me>
2017-06-22 15:32:09 +0000

    Move the benchmarks to a different file and refactor a bit.

---
+++ benchmark_test.go
@@ -0,0 +1,98 @@
+// benchmark_test.go - NewHope benchmarks.
+//
+// To the extent possible under law, Yawning Angel has waived all copyright
+// and related or neighboring rights to newhope, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
+package newhope
+
+import (
+	"bytes"
+	"crypto/rand"
+	"testing"
+)
+
+func benchGenerateKeyPairAlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GenerateKeyPairAlice(rand.Reader)
+	}
+}
+
+func benchKeyExchangeAlice(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		// Generate Alice's key's.
+		alicePriv, alicePub, err := GenerateKeyPairAlice(rand.Reader)
+		if err != nil {
+			b.Fatalf("GenerateKeyPairAlice failed: %v", err)
+		}
+
+		// Finish Bob's handshake.
+		bobPub, bobShared, err := KeyExchangeBob(rand.Reader, alicePub)
+		if err != nil {
+			b.Fatalf("KeyExchangeBob failed: %v", err)
+		}
+
+		// Finish Alice's handshake.
+		b.StartTimer()
+		aliceShared, err := KeyExchangeAlice(bobPub, alicePriv)
+		if err != nil {
+			b.Fatalf("KeyExchangeAlice failed: %v", err)
+		}
+		b.StopTimer()
+
+		// Sanity check.
+		if !bytes.Equal(aliceShared, bobShared) {
+			b.Fatalf("shared secrets mismatched")
+		}
+	}
+}
+
+func benchKeyExchangeBob(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		// Generate Alice's key's.
+		alicePriv, alicePub, err := GenerateKeyPairAlice(rand.Reader)
+		if err != nil {
+			b.Fatalf("GenerateKeyPairAlice failed: %v", err)
+		}
+
+		// Finish Bob's handshake.
+		b.StartTimer()
+		bobPub, bobShared, err := KeyExchangeBob(rand.Reader, alicePub)
+		if err != nil {
+			b.Fatalf("KeyExchangeBob failed: %v", err)
+		}
+		b.StopTimer()
+
+		// Finish Alice's handshake.
+		aliceShared, err := KeyExchangeAlice(bobPub, alicePriv)
+		if err != nil {
+			b.Fatalf("KeyExchangeAlice failed: %v", err)
+		}
+
+		// Sanity check.
+		if !bytes.Equal(aliceShared, bobShared) {
+			b.Fatalf("shared secrets mismatched")
+		}
+	}
+
+}
+
+func BenchmarkNewHope(b *testing.B) {
+	TorSampling = false
+	b.Run("GenerateKeyPairAlice", benchGenerateKeyPairAlice)
+	b.Run("KeyExchangeAlice", benchKeyExchangeAlice)
+	b.Run("KeyExchangeBob", benchKeyExchangeBob)
+}
+
+func BenchmarkNewHopeTor(b *testing.B) {
+	if testing.Short() {
+		b.SkipNow()
+	}
+	TorSampling = true
+	b.Run("GenerateKeyPairAlice", benchGenerateKeyPairAlice)
+	b.Run("KeyExchangeAlice", benchKeyExchangeAlice)
+	b.Run("KeyExchangeBob", benchKeyExchangeBob)
+}
